76714399df582114c22b2cd65a9a670c33984fe0
registerGen <registergen@163.com>
2022-07-24 11:33:28 +0800

    feat(lualine): change time representation

--- lua/plugincfg/lualine.lua
+++ lua/plugincfg/lualine.lua
@@ -30,7 +30,7 @@ M.config = function()
     winbar = {
       lualine_b = { { 'filetype', icon_only = true }, { 'filename', path = 3 } },
       lualine_c = { { 'aerial', sep = ' ❯ ' } },
-      lualine_z = { '%{strftime(\'%c\')}' },
+      lualine_z = { '%{strftime(\'%X\')}' },
     },
     inactive_winbar = {
       lualine_b = { { 'filetype', icon_only = true }, { 'filename', path = 3 } },
