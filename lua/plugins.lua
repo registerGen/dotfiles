c5aa5b982139f03a06116c8879955fede44d6dfb
registerGen <registergen@163.com>
2022-07-26 20:31:00 +0800

    fix(null-ls): check for git dir when registering gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -85,7 +85,16 @@ require('packer').startup(function(use)
       null_ls.setup {
         sources = {
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.code_actions.gitsigns.with {
+            condition = function(utils)
+              for dir in vim.fs.parents(vim.api.nvim_buf_get_name(0)) do
+                if vim.fn.isdirectory(dir .. "/.git") == 1 then
+                  return true
+                end
+              end
+              return false
+            end,
+          },
           null_ls.builtins.diagnostics.zsh,
         },
       }
