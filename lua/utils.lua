30f06c4b910747d187f2daabfad98b307b2a5c4b
registerGen <62944333+registerGen@users.noreply.github.com>
2022-08-06 12:08:34 +0800

    refactor(keymaps): make code easier to maintain (#2)

--- lua/utils.lua
+++ lua/utils.lua
@@ -66,4 +66,22 @@ M.uncomment_config = function()
   end
 end
 
+-- Mappings
+local map_prefix = ''
+
+M.set_map_prefix = function(prefix)
+  map_prefix = prefix
+end
+
+local function create_map_func(mode)
+  return function(lhs, rhs, opts)
+    opts = vim.tbl_deep_extend('keep', {}, { noremap = true, silent = true }, opts)
+    vim.keymap.set(mode, map_prefix .. lhs, rhs, opts)
+  end
+end
+
+M.nmap = create_map_func 'n'
+M.imap = create_map_func 'i'
+M.smap = create_map_func 's'
+
 return M
