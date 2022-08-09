9283c1fc62370dabc489c5f79aca0a5a56a4bd06
registerGen <registergen@163.com>
2022-08-09 19:27:41 +0800

    chore(null-ls): ignore some dirs when attaching gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -82,12 +82,13 @@ require('packer').startup(function(use)
           null_ls.builtins.code_actions.gitsigns.with {
             condition = function(utils)
               local bufname = vim.api.nvim_buf_get_name(0)
+              local exclude_dirs = { os.getenv('HOME') .. '/oi/prob' }
               if vim.fn.isdirectory(bufname .. '/.git') == 1 then
-                return true
+                return not vim.tbl_contains(exclude_dirs, bufname)
               end
               for dir in vim.fs.parents(bufname) do
                 if vim.fn.isdirectory(dir .. '/.git') == 1 then
-                  return true
+                  return not vim.tbl_contains(exclude_dirs, dir)
                 end
               end
               return false
