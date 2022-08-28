6c8ceb68d6d51c6a8e8bbc9bead8ffba3b558954
registerGen <registergen@163.com>
2022-08-28 22:59:18 +0800

    lazy load nvim-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -195,6 +195,7 @@ require('packer').startup(function(use)
   use {
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' },
+    event = 'CursorHold',
     config = function()
       require('plugincfg.nvim_tree').config()
     end,
