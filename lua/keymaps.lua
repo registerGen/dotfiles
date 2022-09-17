957d124a0525268af94fe2b5d1e5846d8325276b
registerGen <registergen@163.com>
2022-09-17 19:16:32 +0800

    style(keymaps.lua): misc

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,18 +9,18 @@ vim.api.nvim_create_autocmd('CursorHold', {
   callback = function()
     local preview = require 'goto-preview'
     u.set_map_prefix '<leader>l'
-    u.nmap('d', function() preview.goto_preview_definition() end, { desc = 'Go to definition' })
-    u.nmap('i', function() preview.goto_preview_implementation() end, { desc = 'Go to implementation' })
-    u.nmap('r', function() preview.goto_preview_references() end, { desc = 'Go to references' })
-    u.nmap('K', function() vim.lsp.buf.hover() end, { desc = 'Hover documentation' })
-    u.nmap('c', function() vim.lsp.buf.code_action() end, { desc = 'Code action' })
-    u.nmap('n', function() vim.lsp.buf.rename() end, { desc = 'Rename' })
-    u.nmap('f', function() vim.lsp.buf.format { async = true } end, { desc = 'Format' })
-    u.nmap('D', function() vim.diagnostic.open_float { focus = false, scope = 'l' } end, { desc = 'Show line diagnostics' })
-    u.nmap('<C-D>', function() vim.diagnostic.open_float { focus = false, scope = 'b' } end, { desc = 'Show buffer diagnostics' })
+    u.nmap('d',      function() preview.goto_preview_definition() end,                        { desc = 'Go to definition' })
+    u.nmap('i',      function() preview.goto_preview_implementation() end,                    { desc = 'Go to implementation' })
+    u.nmap('r',      function() preview.goto_preview_references() end,                        { desc = 'Go to references' })
+    u.nmap('K',      function() vim.lsp.buf.hover() end,                                      { desc = 'Hover documentation' })
+    u.nmap('c',      function() vim.lsp.buf.code_action() end,                                { desc = 'Code action' })
+    u.nmap('n',      function() vim.lsp.buf.rename() end,                                     { desc = 'Rename' })
+    u.nmap('f',      function() vim.lsp.buf.format { async = true } end,                      { desc = 'Format' })
+    u.nmap('D',      function() vim.diagnostic.open_float { focus = false, scope = 'l' } end, { desc = 'Show line diagnostics' })
+    u.nmap('<C-D>',  function() vim.diagnostic.open_float { focus = false, scope = 'b' } end, { desc = 'Show buffer diagnostics' })
     u.set_map_prefix ''
-    u.nmap('[d', function() vim.diagnostic.goto_prev() end, { desc = 'Previous diagnostic' })
-    u.nmap(']d', function() vim.diagnostic.goto_next() end, { desc = 'Next diagnostic' })
+    u.nmap('[d',     function() vim.diagnostic.goto_prev() end,                               { desc = 'Previous diagnostic' })
+    u.nmap(']d',     function() vim.diagnostic.goto_next() end,                               { desc = 'Next diagnostic' })
   end,
 })
 
@@ -29,10 +29,10 @@ u.set_map_prefix ''
 u.nmap('<C-M-N>', '<cmd>RunFile<CR>', { desc = 'Run code' })
 
 -- vsnip
-vim.api.nvim_set_keymap('i', '<C-j>', 'vsnip#jumpable(1)  ? \'<Plug>(vsnip-jump-next)\' : \'<C-j>\'', { expr = true })
-vim.api.nvim_set_keymap('s', '<C-j>', 'vsnip#jumpable(1)  ? \'<Plug>(vsnip-jump-next)\' : \'<C-j>\'', { expr = true })
-vim.api.nvim_set_keymap('i', '<C-k>', 'vsnip#jumpable(-1) ? \'<Plug>(vsnip-jump-prev)\' : \'<C-k>\'', { expr = true })
-vim.api.nvim_set_keymap('s', '<C-k>', 'vsnip#jumpable(-1) ? \'<Plug>(vsnip-jump-prev)\' : \'<C-k>\'', { expr = true })
+vim.api.nvim_set_keymap('i', '<C-j>', [[vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<C-j>']], { expr = true })
+vim.api.nvim_set_keymap('s', '<C-j>', [[vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<C-j>']], { expr = true })
+vim.api.nvim_set_keymap('i', '<C-k>', [[vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>']], { expr = true })
+vim.api.nvim_set_keymap('s', '<C-k>', [[vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>']], { expr = true })
 
 -- iswap
 u.set_map_prefix '<leader>s'
