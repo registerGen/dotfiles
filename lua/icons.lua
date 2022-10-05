2c614d2b31203e51ceb9bda699d487c6ce61a775
registerGen <registergen@163.com>
2022-10-05 19:32:05 +0800

    add icons.lua & pad function

---
+++ lua/icons.lua
@@ -0,0 +1,23 @@
+return {
+  diagnostic = {
+    error = '',
+    warn = '',
+    info = '',
+    hint = '',
+  },
+  git_diff = {
+    added = '',
+    modified = '',
+    removed = '',
+  },
+  arrow = {
+    up = '',
+    down = '',
+    left = '',
+    right = '',
+    top_left = '',
+    top_right = '',
+    bottom_left = '',
+    bottom_right = '',
+  },
+}
