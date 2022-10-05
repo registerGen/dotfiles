b457886a0f8a5567a594105615331da70dbf0c60
registerGen <registergen@163.com>
2022-10-05 17:52:34 +0800

    feat(ufo): customize text

--- lua/plugincfg/ufo.lua
+++ lua/plugincfg/ufo.lua
@@ -2,8 +2,9 @@ local M = {}
 
 local handler = function(virt_text, lnum, end_lnum, width, truncate)
   local new_virt_text = {}
-  local suffix = ('  %d '):format(end_lnum - lnum)
-  local suf_width = vim.fn.strdisplaywidth(suffix)
+  local suffix1 = ' ⋯ '
+  local suffix2 = (' %d '):format(end_lnum - lnum)
+  local suf_width = vim.fn.strdisplaywidth(suffix1 .. suffix2)
   local target_width = width - suf_width
   local cur_width = 0
   for _, chunk in ipairs(virt_text) do
@@ -16,15 +17,15 @@ local handler = function(virt_text, lnum, end_lnum, width, truncate)
       local hlgroup = chunk[2]
       table.insert(new_virt_text, { chunk_text, hlgroup })
       chunk_width = vim.fn.strdisplaywidth(chunk_text)
-      -- str width returned from truncate() may less than 2nd argument, need padding
       if cur_width + chunk_width < target_width then
-        suffix = suffix .. (' '):rep(target_width - cur_width - chunk_width)
+        suffix1 = suffix1 .. (' '):rep(target_width - cur_width - chunk_width)
       end
       break
     end
     cur_width = cur_width + chunk_width
   end
-  table.insert(new_virt_text, { suffix, 'MoreMsg' })
+  table.insert(new_virt_text, { suffix1, 'UfoFoldedEllipsis' })
+  table.insert(new_virt_text, { suffix2, 'MoreMsg' })
   return new_virt_text
 end
 
