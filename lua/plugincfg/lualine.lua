7e47f99522e703e613a108942f7cbc136ada3c27
registerGen <registergen@163.com>
2022-10-10 21:53:28 +0800

    feat(lualine): hide section_b and filename on cmd mode
    This reverts commit c585351f728319c4e56c622e6bacbc8795d9dcdd.

--- lua/plugincfg/lualine.lua
+++ lua/plugincfg/lualine.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+local function in_cmd_mode()
+  return vim.api.nvim_get_mode().mode == 'c'
+end
+
 M.config = function()
   local configuration = vim.fn['sonokai#get_configuration']()
   local palette = vim.fn['sonokai#get_palette'](configuration.style, configuration.colors_override)
@@ -9,9 +13,20 @@ M.config = function()
       globalstatus = true,
     },
     sections = {
+      lualine_a = { 'mode' },
       lualine_b = {
-        'branch',
-        'diff',
+        {
+          'branch',
+          cond = function()
+            return not in_cmd_mode()
+          end,
+        },
+        {
+          'diff',
+          cond = function()
+            return not in_cmd_mode()
+          end,
+        },
         {
           'diagnostics',
           sources = { 'nvim_diagnostic' },
@@ -23,15 +38,23 @@ M.config = function()
             info = { fg = palette.blue[1] },
             hint = { fg = palette.green[1] },
           },
+          cond = function()
+            return not in_cmd_mode()
+          end,
         },
       },
       lualine_c = {
-        'filename',
+        {
+          'filename',
+          cond = function()
+            return not in_cmd_mode()
+          end,
+        },
         {
           '%{getcmdline()}',
           color = { fg = palette.yellow[1] },
           cond = function()
-            return vim.api.nvim_get_mode().mode == 'c'
+            return in_cmd_mode()
           end,
         },
       },
