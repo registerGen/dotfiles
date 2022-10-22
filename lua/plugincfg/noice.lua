b71c321403b93ec5018b5b2dc9c8657032c6e8da
registerGen <registergen@163.com>
2022-10-22 18:34:49 +0800

    feat(noice)!: update config

--- lua/plugincfg/noice.lua
+++ lua/plugincfg/noice.lua
@@ -2,43 +2,12 @@ local M = {}
 
 M.config = function()
   require('noice').setup {
-    cmdline = {
-      icons = {
-        ['/'] = { icon = '', hl_group = 'NormalFloat' },
-        ['?'] = { icon = '', hl_group = 'NormalFloat' },
-        [':'] = { icon = '', hl_group = 'NormalFloat', firstc = false },
-      },
-    },
-    popupmenu = {
-      backend = 'cmp',
-    },
     views = {
       cmdline_popup = {
         position = {
           row = '30%',
         },
-        win_options = {
-          winhighlight = { NormalFloat = 'NormalFloat', FloatBorder = 'FloatBorder' },
-        },
-        filter_options = {
-          {
-            filter = { event = 'cmdline', find = '^%s*[/?]' },
-            opts = {
-              border = {
-                text = {
-                  top = ' Search ',
-                },
-              },
-              win_options = {
-                winhighlight = { NormalFloat = 'NormalFloat', FloatBorder = 'FloatBorder' },
-              },
-            },
-          },
-        },
       },
-    },
-    notify = {
-      enabled = false,
     },
   }
 end
