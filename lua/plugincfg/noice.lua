b11eba8b52ce35d6fd8292de4351bc48db1470ee
registerGen <registergen@163.com>
2022-10-28 22:52:13 +0800

    style: format with stylua

--- lua/plugincfg/noice.lua
+++ lua/plugincfg/noice.lua
@@ -8,9 +8,9 @@ M.config = function()
     },
     lsp = {
       override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
       hover = {
         enabled = true,
@@ -20,7 +20,7 @@ M.config = function()
       },
       message = {
         enabled = true,
-      }
+      },
     },
   }
 end
