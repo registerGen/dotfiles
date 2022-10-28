8328291beb1710d9a66870c09c267b2a2e40c0f3
registerGen <registergen@163.com>
2022-10-28 23:24:55 +0800

    feat(noice)!: keep up with new features

--- lua/plugincfg/noice.lua
+++ lua/plugincfg/noice.lua
@@ -5,21 +5,13 @@ M.config = function()
     presets = {
       command_palette = true,
       inc_rename = true,
+      lsp_doc_border = true,
     },
     lsp = {
       override = {
         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
         ['vim.lsp.util.stylize_markdown'] = true,
         ['cmp.entry.get_documentation'] = true,
-      },
-      hover = {
-        enabled = true,
-      },
-      signature = {
-        enabled = true,
-      },
-      message = {
-        enabled = true,
       },
     },
   }
