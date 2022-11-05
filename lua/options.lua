9d38e74e19b87fac8bbf17248cfcb06d2d6605e8
registerGen <registergen@163.com>
2022-11-05 23:33:08 +0800

    fix: true color in tmux

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,10 @@
 local opt, opt_local = vim.opt, vim.opt_local
 
+vim.cmd [[
+  let &t_8f = '\<Esc>[38;2;%lu;%lu;%lum'
+  let &t_8b = '\<Esc>[48;2;%lu;%lu;%lum'
+]]
+
 opt.background = 'dark'
 opt.backup = false
 opt.cmdheight = 0
