eda92bc7c49ecd6097c5d5745b0facf3b342c862
registerGen <registergen@163.com>
2022-11-05 19:35:18 +0800

    chore(sonokai): remove unused hlgroups

--- lua/plugincfg/sonokai.lua
+++ lua/plugincfg/sonokai.lua
@@ -17,15 +17,6 @@ M.config = function()
         vim.fn['sonokai#get_palette'](configuration.style, configuration.colors_override)
       vim.fn['sonokai#highlight']('TSParameter', palette.orange, palette.none, 'italic')
       vim.fn['sonokai#highlight']('TSParameterReference', palette.orange, palette.none, 'italic')
-
-      -- wilder.nvim
-      vim.cmd [[hi! link WilderDefault Pmenu]]
-      vim.cmd [[hi! link WilderSelected PmenuSel]]
-      vim.cmd [[hi! link WilderAccent CmpItemAbbrMatch]]
-      vim.fn['sonokai#highlight']('WilderSelectedAccent', palette.bg0, palette.bg_blue, 'bold')
-
-      -- blamer.nvim
-      vim.cmd [[hi! link Blamer Comment]]
     end,
   })
   vim.cmd.colors 'sonokai'
