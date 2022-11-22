e096929d7a9a92ead7277e7df8123102bf792bd8
registerGen <registergen@163.com>
2022-11-22 16:59:58 +0800

    refactor(nvim): use `vim.diagnostic.config()` to config diagnostic

--- nvim/lua/plugincfg/lsp/ui.lua
+++ nvim/lua/plugincfg/lsp/ui.lua
@@ -3,12 +3,16 @@ local M = {}
 local icons = require 'icons'
 
 M.on_attach = function(_, bufnr)
-  vim.lsp.handlers['textDocument/publishDiagnostics'] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      update_in_insert = true,
-      virtual_text = { spacing = 4 },
-      severity_sort = true,
-    })
+  vim.diagnostic.config {
+    virtual_text = {
+      spacing = 4,
+    },
+    float = {
+      border = 'rounded',
+    },
+    update_in_insert = true,
+    severity_sort = true,
+  }
 
   local signs = {
     Error = icons.diagnostic.error,
@@ -19,13 +23,6 @@ M.on_attach = function(_, bufnr)
   for type, icon in pairs(signs) do
     local hl = 'DiagnosticSign' .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
-
-  local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-  function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = opts or {}
-    opts.border = 'rounded'
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
   end
 
   local id = vim.api.nvim_create_augroup('LspDocumentHighlight', { clear = true })
