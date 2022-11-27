7f2b46a5370dc826fab31729b7066a3fb4551b68
registerGen <registergen@163.com>
2022-11-27 13:20:33 +0800

    feat(nvim): disable border in codewindow

--- nvim/lua/plugincfg/codewindow.lua
+++ nvim/lua/plugincfg/codewindow.lua
@@ -4,6 +4,7 @@ M.config = function()
   local codewindow = require 'codewindow'
   codewindow.setup {
     exclude_filetypes = { 'dashboard', 'startuptime', 'gitcommit', 'NvimTree', 'help', '' },
+    window_border = '',
   }
   codewindow.apply_default_keybinds()
 end
