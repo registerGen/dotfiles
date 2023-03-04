f9ee81044dbd5bb35e8b7f44abdf08c1ac37c137
registerGen <registergen@163.com>
2023-03-04 16:04:54 +0800

    fix(nvim)!: follow upstream changes of statuscol.nvim

--- nvim/lua/plugincfg/statuscol.lua
+++ nvim/lua/plugincfg/statuscol.lua
@@ -1,9 +1,13 @@
 local M = {}
 
 M.config = function()
+  local builtin = require 'statuscol.builtin'
   require('statuscol').setup {
-    foldfunc = 'builtin',
-    order = 'SNsFs',
+    segments = {
+      { text = { '%s' }, click = 'v:lua.ScSa' },
+      { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
+      { text = { ' ', builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
+    },
     setopt = true,
   }
 end
