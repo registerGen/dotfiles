7d6134775542c813080d169d6e6fc1937224217f
registerGen <registergen@163.com>
2023-03-17 21:04:47 +0800

    feat(nvim): add git diff navigation keymaps

--- nvim/lua/plugincfg/gitsigns.lua
+++ nvim/lua/plugincfg/gitsigns.lua
@@ -1,11 +1,35 @@
 local M = {}
 
+local gs = require 'gitsigns'
+local u = require 'utils'
+
 M.config = function()
-  require('gitsigns').setup {
+  gs.setup {
     signcolumn = true,
     numhl = true,
     current_line_blame = true,
   }
+
+  u.set_map_prefix ''
+  u.nmap(']h', function()
+    if vim.wo.diff then
+      return ']h'
+    end
+    vim.schedule(function()
+      gs.next_hunk()
+    end)
+    return '<Ignore>'
+  end, { expr = true, desc = 'Next git diff hunk' })
+
+  u.nmap('[h', function()
+    if vim.wo.diff then
+      return '[h'
+    end
+    vim.schedule(function()
+      gs.prev_hunk()
+    end)
+    return '<Ignore>'
+  end, { expr = true, desc = 'Previous git diff hunk' })
 end
 
 return M
