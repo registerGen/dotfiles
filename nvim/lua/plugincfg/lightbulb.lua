a70d263513ce7be4c596c902c25ff5f41fef25b3
registerGen <registergen@163.com>
2023-07-15 20:24:39 +0800

    fix(nvim)!: follow upstream change of nvim-lightbulb

--- nvim/lua/plugincfg/lightbulb.lua
+++ nvim/lua/plugincfg/lightbulb.lua
@@ -4,21 +4,13 @@ local icons = require 'icons'
 
 M.config = function()
   require('nvim-lightbulb').setup {
+    autocmd = {
+      enabled = true,
+    },
     sign = {
-      priority = 100,
+      text = icons.misc.bulb,
     },
   }
-
-  vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-    callback = function()
-      require('nvim-lightbulb').update_lightbulb()
-    end,
-  })
-
-  vim.fn.sign_define(
-    'LightBulbSign',
-    { text = icons.misc.bulb, texthl = 'DiagnosticSignInformation' }
-  )
 end
 
 return M
