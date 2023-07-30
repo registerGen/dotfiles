c6ddabdcca094d4148e7e44e1ab200ff22fe6e26
registerGen <registergen@163.com>
2023-07-30 19:45:09 +0800

    feat(nvim): transparent clock

--- nvim/lua/plugincfg/clock.lua
+++ nvim/lua/plugincfg/clock.lua
@@ -9,6 +9,8 @@ for _, group_name in pairs({ "Red", "Orange", "Yellow", "Green", "Blue", "Purple
   r[#r + 1] = bit.rshift(fg, 16)
   g[#g + 1] = bit.band(bit.rshift(fg, 8), 255)
   b[#b + 1] = bit.band(fg, 255)
+
+  api.nvim_set_hl(0, "Clock" .. group_name, { fg = fg, bg = "bg" })
 end
 
 for i = 1, 6, 1 do
@@ -19,15 +21,20 @@ for i = 1, 6, 1 do
     G = math.floor(g[i] * (7 - j) / 7 + g[i + 1] * j / 7 + 0.5)
     B = math.floor(b[i] * (7 - j) / 7 + b[i + 1] * j / 7 + 0.5)
     local value = string.format("#%X%X%X", R, G, B)
-    api.nvim_set_hl(0, group_name, { fg = value })
+
+    api.nvim_set_hl(0, group_name, { fg = value, bg = "bg" })
   end
 end
 
 M.config = function()
   require("clock").setup({
+    float = {
+      border = "none",
+      padding = { 0, 0, 0, 0 },
+    },
     hl_group_pixel = function(c, _, position, _, pixel_col)
       if c == ":" then
-        return position == 3 and "Yellow" or "Blue"
+        return position == 3 and "ClockYellow" or "ClockBlue"
       end
 
       if position >= 7 then
@@ -38,6 +45,7 @@ M.config = function()
 
       return "Clock" .. tostring(position) .. tostring(pixel_col)
     end,
+    separator_hl = "Normal",
   })
 end
 
