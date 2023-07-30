c7f330df83881b7d86a3a25cc67d67d7ded7aa0e
registerGen <registergen@163.com>
2023-07-30 15:55:17 +0800

    feat(clock): more fancy clock

--- nvim/lua/plugincfg/clock.lua
+++ nvim/lua/plugincfg/clock.lua
@@ -1,33 +1,42 @@
 local M = {}
 
-local hl_group = {
-  "Red",
-  "Orange",
-  "Grey",
-  "Yellow",
-  "Green",
-  "Grey",
-  "Blue",
-  "Purple",
-}
+local api = vim.api
 
-local function update_hl_group()
-  local new_hl_group = vim.deepcopy(hl_group)
-  new_hl_group[1] = hl_group[8]
-  new_hl_group[2] = hl_group[1]
-  new_hl_group[4] = hl_group[2]
-  new_hl_group[5] = hl_group[4]
-  new_hl_group[7] = hl_group[5]
-  new_hl_group[8] = hl_group[7]
-  hl_group = new_hl_group
+local r, g, b = {}, {}, {}
+
+for _, group_name in pairs({ "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Red" }) do
+  local fg = api.nvim_get_hl(0, { name = group_name }).fg
+  r[#r + 1] = bit.rshift(fg, 16)
+  g[#g + 1] = bit.band(bit.rshift(fg, 8), 255)
+  b[#b + 1] = bit.band(fg, 255)
 end
 
--- assert(vim.loop.new_timer()):start(500, 500, update_hl_group)
+for i = 1, 6, 1 do
+  for j = 1, 6, 1 do
+    local group_name = "Clock" .. tostring(i) .. tostring(j)
+    local R, G, B
+    R = math.floor(r[i] * (7 - j) / 6 + r[i + 1] * (j - 1) / 6)
+    G = math.floor(g[i] * (7 - j) / 6 + g[i + 1] * (j - 1) / 6)
+    B = math.floor(b[i] * (7 - j) / 6 + b[i + 1] * (j - 1) / 6)
+    local value = string.format("#%X%X%X", R, G, B)
+    api.nvim_set_hl(0, group_name, { fg = value })
+  end
+end
 
 M.config = function()
   require("clock").setup({
-    hl_group = function(_, _, position)
-      return hl_group[position]
+    hl_group_pixel = function(c, _, position, _, pixel_col)
+      if c == ":" then
+        return "Grey"
+      end
+
+      if position >= 7 then
+        position = position - 2
+      elseif position >= 4 then
+        position = position - 1
+      end
+
+      return "Clock" .. tostring(position) .. tostring(pixel_col)
     end,
   })
 end
