0fcf32f84531548d342835a77f943562962ad136
registerGen <registergen@163.com>
2023-08-19 21:48:51 +0800

    feat(nvim): enable semantic tokens

--- nvim/lua/plugincfg/lsp/init.lua
+++ nvim/lua/plugincfg/lsp/init.lua
@@ -24,12 +24,12 @@ M.config = function()
     }, config))
   end
 
-  vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      client.server_capabilities.semanticTokensProvider = nil
-    end,
-  })
+  -- vim.api.nvim_create_autocmd("LspAttach", {
+  --   callback = function(args)
+  --     local client = vim.lsp.get_client_by_id(args.data.client_id)
+  --     client.server_capabilities.semanticTokensProvider = nil
+  --   end,
+  -- })
 
   vim.cmd("LspStart")
 end
--- nvim/lua/plugincfg/sonokai.lua
+++ nvim/lua/plugincfg/sonokai.lua
@@ -15,6 +15,31 @@ M.config = function()
       vim.api.nvim_set_hl(0, "TSParameter", { link = "OrangeItalic", default = false })
       vim.api.nvim_set_hl(0, "TSParameterReference", { link = "OrangeItalic", default = false })
       vim.api.nvim_set_hl(0, "NoicePopupmenuMatch", { link = "CmpItemAbbrMatch", default = false })
+      vim.cmd [[
+        hi! link @lsp.type.class         TSClass
+        hi! link @lsp.type.comment       TSComment
+        hi! link @lsp.type.decorator     TSFunction
+        hi! link @lsp.type.enum          TSType
+        hi! link @lsp.type.enumMember    TSConstMacro
+        hi! link @lsp.type.events        TSLabel
+        hi! link @lsp.type.function      TSFunction
+        hi! link @lsp.type.interface     TSType
+        hi! link @lsp.type.keyword       TSKeyword
+        hi! link @lsp.type.macro         TSConstMacro
+        hi! link @lsp.type.method        TSMethod
+        hi! link @lsp.type.modifier      TSTypeQualifier
+        hi! link @lsp.type.namespace     TSNamespace
+        hi! link @lsp.type.number        TSNumber
+        hi! link @lsp.type.operator      TSOperator
+        hi! link @lsp.type.parameter     TSParameter
+        hi! link @lsp.type.property      TSProperty
+        hi! link @lsp.type.regexp        TSStringRegex
+        hi! link @lsp.type.string        TSString
+        hi! link @lsp.type.struct        TSType
+        hi! link @lsp.type.type          TSType
+        hi! link @lsp.type.typeParameter TSTypeDefinition
+        hi! link @lsp.type.variable      TSVariable
+      ]]
     end,
   })
   vim.cmd.colors("sonokai")
