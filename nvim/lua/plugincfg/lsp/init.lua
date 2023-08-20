d7bdf1b76b76db149b7bf362e3b10624176be305
registerGen <registergen@163.com>
2023-08-20 20:40:17 +0800

    feat(nvim): disable hlargs if the server supports semantic tokens
    Also refactor the code.

--- nvim/lua/plugincfg/lsp/init.lua
+++ nvim/lua/plugincfg/lsp/init.lua
@@ -1,8 +1,10 @@
 local M = {}
 
 M.config = function()
+  -- Setup neodev
   require("neodev").setup()
 
+  -- Setup lspconfig
   for server, config in pairs(require("plugincfg.lsp.servers")) do
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -16,20 +18,36 @@ M.config = function()
       on_attach = function(client, bufnr)
         require("plugincfg.lsp.ui").on_attach(client, bufnr)
         require("plugincfg.lsp.keymaps").apply_keymaps()
-
-        if client.server_capabilities.inlayHintProvider then
-          vim.lsp.inlay_hint(bufnr, true)
-        end
       end,
     }, config))
   end
 
-  -- vim.api.nvim_create_autocmd("LspAttach", {
-  --   callback = function(args)
-  --     local client = vim.lsp.get_client_by_id(args.data.client_id)
-  --     client.server_capabilities.semanticTokensProvider = nil
-  --   end,
-  -- })
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LspConfig", { clear = true }),
+    callback = function(args)
+      if not (args.data and args.data.client_id) then
+        return
+      end
+
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      local caps = assert(client.server_capabilities)
+
+      -- Disable hlargs if the server has semantic tokens capability
+      if caps.semanticTokensProvider then
+        require("hlargs").disable_buf(args.buf)
+        vim.api.nvim_create_autocmd("InsertEnter", {
+          callback = function()
+            require("hlargs").disable_buf(args.buf)
+          end,
+        })
+      end
+
+      -- Enable inlay hints if the server support this
+      if caps.inlayHintProvider then
+        vim.lsp.inlay_hint(args.buf, true)
+      end
+    end,
+  })
 
   vim.cmd("LspStart")
 end
