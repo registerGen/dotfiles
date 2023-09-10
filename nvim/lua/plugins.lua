1216f92f74d47c97ebb764f583c5614d81c1f0d9
registerGen <registergen@163.com>
2023-09-10 13:06:20 +0800

    feat(nvim)!: remove flash.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -265,23 +265,6 @@ require("lazy").setup({
     end,
   },
 
-  -- Motion {{{1
-  {
-    "folke/flash.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end,
-                                                                                                  desc =
-        "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc =
-      "Toggle Flash Search" },
-    },
-  },
-
   -- Code Runner {{{1
   {
     "CRAG666/code_runner.nvim",
