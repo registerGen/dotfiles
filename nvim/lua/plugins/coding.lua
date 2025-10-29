0e264221143e9b62ebccdd61bde92b03c7b9c2eb
registerGen <registergen@163.com>
2025-10-29 15:26:44 +0800

    update

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -53,13 +53,8 @@ return {
   {
     "ggandor/leap.nvim",
     config = function()
-      require("leap").set_default_mappings()
-      require("leap").opts.preview_filter = function(ch0, ch1, ch2)
-        return not (
-          ch1:match("%s") or
-          ch0:match("%a") and ch1:match("%a") and ch2:match("%a")
-        )
-      end
+      vim.keymap.set({"n", "x", "o"}, "s", "<Plug>(leap)")
+      vim.keymap.set("n",             "S", "<Plug>(leap-from-window)")
     end,
   }
 }
