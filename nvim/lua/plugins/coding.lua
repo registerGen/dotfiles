a9ba0be7c363b43362b37cee00f89dd2743a2a48
registerGen <registergen@163.com>
2026-02-11 21:28:05 +0800

    update

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -52,10 +52,13 @@ return {
     end
   },
   {
-    "ggandor/leap.nvim",
+    url = "https://codeberg.org/andyg/leap.nvim",
     config = function()
       vim.keymap.set({"n", "x", "o"}, "s", "<Plug>(leap)")
       vim.keymap.set("n",             "S", "<Plug>(leap-from-window)")
     end,
-  }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context"
+  },
 }
