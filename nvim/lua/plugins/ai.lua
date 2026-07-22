5f87940fafd40bf729cf943aa057cef4855035cf
registerGen <xcs25@mails.tsinghua.edu.cn>
2026-07-22 15:56:10 +0800

    update

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    dependencies = {
-      "copilotlsp-nvim/copilot-lsp",
-    },
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      panel = { enabled = false },
-      suggestion = {
-        auto_trigger = true,
-        enabled = true,
-        keymap = {
-          accept = "<C-a>",
-        },
-      },
-      nes = {
-        auto_trigger = true,
-        enabled = true,
-        keymap = {
-          accept = "<M-a>",
-        },
-      }
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   dependencies = {
+  --     "copilotlsp-nvim/copilot-lsp",
+  --   },
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   opts = {
+  --     panel = { enabled = false },
+  --     suggestion = {
+  --       auto_trigger = true,
+  --       enabled = true,
+  --       keymap = {
+  --         accept = "<C-a>",
+  --       },
+  --     },
+  --     nes = {
+  --       auto_trigger = true,
+  --       enabled = true,
+  --       keymap = {
+  --         accept = "<M-a>",
+  --       },
+  --     }
+  --   },
+  -- },
 }
