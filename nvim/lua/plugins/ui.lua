58d89f132ea7357c4fb71569e8b073131d8bcdcc
registerGen <xcs25@mails.tsinghua.edu.cn>
2026-08-23 00:30:10 +0800

    update

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -81,34 +81,7 @@ return {
   },
   {
     "kevinhwang91/nvim-ufo",
-    opts = {
-      fold_virt_text_handler = function(virt_text, lnum, end_lnum, width, truncate)
-        local new_virt_text = {}
-        local suffix = (" %s %d "):format(icons.arrow.bottom_left, end_lnum - lnum)
-        local suf_width = vim.fn.strdisplaywidth(suffix)
-        local target_width = width - suf_width
-        local cur_width = 0
-        for _, chunk in ipairs(virt_text) do
-          local chunk_text = chunk[1]
-          local chunk_width = vim.api.nvim_strwidth(chunk_text)
-          if target_width > cur_width + chunk_width then
-            table.insert(new_virt_text, chunk)
-          else
-            chunk_text = truncate(chunk_text, target_width - cur_width)
-            local hl_group = chunk[2]
-            table.insert(new_virt_text, { chunk_text, hl_group })
-            chunk_width = vim.api.nvim_strwidth(chunk_text)
-            if cur_width + chunk_width < target_width then
-              suffix = suffix .. (" "):rep(target_width - cur_width - chunk_width)
-            end
-            break
-          end
-          cur_width = cur_width + chunk_width
-        end
-        table.insert(new_virt_text, { suffix, "MoreMsg" })
-        return new_virt_text
-      end,
-    },
+    opts = {},
     config = function(_, opts)
       require("ufo").setup(opts)
       vim.keymap.set("n", "zR", require("ufo").openAllFolds)
